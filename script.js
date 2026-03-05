2f1b2761fbeaf68cd8e68f171dde3451bb5b71fe
Isaque <isaqueaf9307286@gmail.com>
2026-03-05 00:07:51 -0300

    carrinho

--- script.js
+++ script.js
@@ -22,6 +22,49 @@ const adicionarCarrinho = function (produto)
     localStorage.setItem(produto, Number(localStorage.getItem(produto)) + 1);
 }
 
+const atualizarTotal = function ()
+{
+    const produtos = obterProdutos();
+    let total = 0;
+
+    produtos.forEach(element => {
+        const produto = Produtos[Produtos.findIndex(item => item[0] === element[0])];
+        total += produto[1] * Number(element[1]);
+    });
+
+    document.querySelector('.resumo h2').textContent = `Total: R$ ${total.toFixed(2)}`;
+}
+
+const atualizarQuantidade = function (produto)
+{
+    const quantidade = Number(localStorage.getItem(produto));
+    document.querySelector(`.quantidade_${Produtos.findIndex(item => item[0] === produto)}`).textContent = quantidade;
+}
+
+const adicionarProduto = function (produto)
+{
+    localStorage.setItem(produto, Number(localStorage.getItem(produto)) + 1);
+    atualizarQuantidade(produto);
+    atualizarTotal();
+}
+
+const retirarProduto = function (produto)
+{
+    if (Number(localStorage.getItem(produto)) > 1)
+    {
+        localStorage.setItem(produto, Number(localStorage.getItem(produto)) - 1);
+        atualizarQuantidade(produto);
+        atualizarTotal();
+    }
+}
+
+const removerProduto = function (produto)
+{
+    localStorage.removeItem(produto);
+    document.querySelector(`.item_carrinho_${Produtos.findIndex(item => item[0] === produto)}`).remove();
+    atualizarTotal();
+}
+
 const obterProdutos = function ()
 {
     const produtos = []
